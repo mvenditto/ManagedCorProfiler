bd31aa6067d74e7e38732d484e0f191834bf4a2a
mvenditto <venditto.matteo@gmail.com>
2023-01-23 21:41:38 +0100

    added ShutdownGuard

---
+++ CorProf.Shared/ShutdownGuard.cs
@@ -0,0 +1,32 @@
+﻿namespace CorProf.Shared
+{
+    public class ShutdownGuard: IDisposable
+    {
+        private static CountdownEvent _hooksInProgress = new(0);
+
+        public static bool HasShutdownStarted()
+        {
+            return _hooksInProgress.CurrentCount == 0;
+        }
+
+        public static void WaitForInProgressHooks()
+        {
+            _hooksInProgress.Wait();
+        }
+
+        public static void Initialize()
+        {
+            _hooksInProgress.Reset();
+        }
+
+        public ShutdownGuard()
+        {
+            _hooksInProgress.AddCount(1);
+        }
+
+        public void Dispose()
+        {
+            _hooksInProgress.Signal(1);
+        }
+    }
+}
