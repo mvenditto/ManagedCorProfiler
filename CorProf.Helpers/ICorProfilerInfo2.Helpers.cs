ffa144e4bd0ca4631ca59baff1818750bfe68c95
mvenditto <venditto.matteo@gmail.com>
2023-01-29 20:45:37 +0100

    GetFunctionIDFullyQualifiedName

--- CorProf.Helpers/ICorProfilerInfo2.Helpers.cs
+++ CorProf.Helpers/ICorProfilerInfo2.Helpers.cs
@@ -1,4 +1,5 @@
 ﻿using CorProf.Bindings;
+using System.Runtime.InteropServices;
 
 namespace CorProf.Helpers
 {
@@ -21,8 +22,14 @@ namespace CorProf.Helpers
             _profInfo = profilerInfo;
         }
 
-        public int GetFunctionIDName(ulong funcId, out string funcName)
+        public int GetFunctionIDFullyQualifiedName(ulong funcId, out string funcName)
         {
+            if (funcId == 0)
+            {
+                funcName = "Unknown_Native_Function";
+                return 0;
+            }
+
             ulong classId = 0;
             ulong moduleId = 0;
             uint token = 0;
@@ -45,8 +52,35 @@ namespace CorProf.Helpers
             if (hr < 0)
             {
                 Console.WriteLine($"Error GetFunctionInfo2 hr={hr}");
+                funcName = "FuncNameLookupFailed";
+                return hr;
             }
 
+            var pbBaseLoadAddr = (byte*)null;
+            uint pcchName = 0;
+            ulong assemblyId = 0;
+
+            using var szName = NativeBuffer<ushort>.Alloc(300);
+
+            hr = _profInfo->GetModuleInfo(
+                moduleId,
+                &pbBaseLoadAddr,
+                300,
+                &pcchName,
+                szName,
+                &assemblyId);
+            if (hr < 0)
+            {
+                Console.WriteLine($"HR: 0x{hr:x8}");
+            }
+
+            string modName = MarshalHelpers.PtrToStringUtf16(szName) ?? "";
+            
+            if (modName != "")
+            {
+                modName = Path.GetFileName(modName);
+            }
+            
             var iMetaDataImportIID = new Guid(0x7dac8207, 0xd3ae, 0x4c75, 0x9b, 0x67, 0x92, 0x80, 0x1a, 0x49, 0x7d, 0x44);
 
             var ptr = NativeBuffer.Alloc((nuint)sizeof(nint));
@@ -64,7 +98,7 @@ namespace CorProf.Helpers
 
             var metadataImport = (IMetaDataImport*)ptr;
 
-            using var szName = NativeBuffer<ushort>.Alloc(300);
+            // using var szName = NativeBuffer<ushort>.Alloc(300);
 
             uint typeDefToken = 0;
 
@@ -73,11 +107,14 @@ namespace CorProf.Helpers
 
             string name = MarshalHelpers.PtrToStringUtf16(szName) ?? "???";
 
+            metadataImport->GetTypeDefProps(typeDefToken, szName, 300, null, null, null);
+
+            string clsName = MarshalHelpers.PtrToStringUtf16(szName) ?? "???";
+
             if (nTypeArgs > 0)
             {
                 name += "<";
             }
-
 
             for (uint i = 0; i < nTypeArgs; i++)
             {
@@ -96,7 +133,7 @@ namespace CorProf.Helpers
                 name += ">";
             }
 
-            funcName = name;
+            funcName = modName + "!" + clsName + "::" + name;
 
             return S_OK;
         }
